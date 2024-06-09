a6982ed69f183371487daa98811fccb88d9a2a0b
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-06-09 07:17:20 +0100

    chore: remove second :ss

--- frontend/src/common/formatter.ts
+++ frontend/src/common/formatter.ts
@@ -2,22 +2,22 @@ import dayjs from "dayjs"
 import { format } from "numerable"
 
 function formatDate(value: Date, withTime: boolean = true) {
-    const format = withTime ? 'DD/MM/YYYY hh:mm:ss' : 'DD/MM/YYYY'
+    const format = withTime ? 'DD/MM/YYYY hh:mm' : 'DD/MM/YYYY'
     return dayjs(value).format(format)
 }
 
 function formatWeekDay(value: Date, withTime: boolean = true) {
-    const format = withTime ? 'dddd DD/MM/YYYY hh:mm:ss' : 'dddd DD/MM/YYYY'
+    const format = withTime ? 'dddd DD/MM/YYYY hh:mm' : 'dddd DD/MM/YYYY'
     return dayjs(value).format(format)
 }
 
 function formatTime(value: Date) {
-    return dayjs(value).format('hh:mm:ss A')
+    return dayjs(value).format('hh:mm A')
 }
 
 function formatDateRange(start: Date, end: Date) {
     if (dayjs(start).isSame(end, 'date')) {
-        return `${dayjs(start).format('DD/MM/YYYY')} ${dayjs(start).format('hh:mm:ss A')} - ${dayjs(end).format('hh:mm:ss A')}`
+        return `${dayjs(start).format('DD/MM/YYYY')} ${dayjs(start).format('hh:mm A')} - ${dayjs(end).format('hh:mm A')}`
     }
     return `${formatDate(start)} - ${formatDate(end)}`
 }
