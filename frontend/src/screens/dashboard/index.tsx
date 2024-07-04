e45d59c347d1fcb14b0df0c96ec365eee7d27815
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-07-04 06:41:26 +0100

    Load past 1 month matches

--- frontend/src/screens/dashboard/index.tsx
+++ frontend/src/screens/dashboard/index.tsx
@@ -19,17 +19,29 @@ function Dashboard() {
     return matches.filter((m) => dayjs(m.start).isAfter(new Date(), "date"));
   }, [matches]);
 
+  const historyMatches = useMemo(() => {
+    if (!matches) return null;
+    return matches.filter((m) =>
+      dayjs(m.start).isAfter(dayjs(new Date()).subtract(1, "month")),
+    );
+  }, [matches]);
+
   return (
     <Page title="Dashboard">
       <div className="flex flex-col gap-2">
         <Suspense fallback={<Loading />}>
           {todayMatches && (
-            <MatchSection title="Today Match" matches={todayMatches} />
+            <MatchSection title="Today" matches={todayMatches} />
           )}
         </Suspense>
         <Suspense fallback={<Loading />}>
           {upcomingMatches && (
-            <MatchSection title="Upcoming Matches" matches={upcomingMatches} />
+            <MatchSection title="Upcoming" matches={upcomingMatches} />
+          )}
+        </Suspense>
+        <Suspense fallback={<Loading />}>
+          {historyMatches && (
+            <MatchSection title="Last month" matches={historyMatches} />
           )}
         </Suspense>
       </div>
--- frontend/src/screens/dashboard/match-list-content.tsx
+++ frontend/src/screens/dashboard/match-list-content.tsx
@@ -13,6 +13,7 @@ import {
   IoCopy,
   IoHeartCircle,
   IoPersonSharp,
+  IoPin,
   IoTime,
 } from "react-icons/io5";
 import Markdown from "react-markdown";
@@ -33,12 +34,15 @@ import {
 } from "../../models";
 import AdditionalCostEditor from "./additional-cost-editor";
 import MatchFigure from "./match-figure";
+import dayjs from "dayjs";
 
 interface Prop {
   match: MatchSummaryModel;
 }
 
 const MatchListContent: React.FC<Prop> = ({ match }) => {
+  const clipboardLoc = useClipboard({ timeout: 500 });
+  const clipboardRefLoc = useRef<HTMLDivElement>(null!);
   const clipboard = useClipboard({ timeout: 500 });
   const clipboardRef = useRef<HTMLDivElement>(null!);
 
@@ -146,7 +150,41 @@ const MatchListContent: React.FC<Prop> = ({ match }) => {
     <div className="flex flex-col gap-3">
       <div className="grid grid-cols-1 gap-3 xl:grid-cols-2">
         <div className="flex flex-col gap-3">
-          <div>
+          <div className="flex flex-col gap-3">
+            <Alert
+              className="relative"
+              color="red"
+              title="Match Details"
+              icon={<IoPin />}
+            >
+              <div className="flex flex-col gap-2" ref={clipboardRefLoc}>
+                <div className="flex items-center gap-2 font-bold">
+                  <span>Match open at {match.sportCenterName}</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span>
+                    <span>📆 </span>
+                    {dayjs(match.start).format("dddd DD-MM-YYYY")}
+                    <span> ⏰ </span>
+                    {dayjs(match.start).format("hh:mm")}
+                    <span> to </span>
+                    {dayjs(match.end).format("hh:mm")}
+                  </span>
+                </div>
+              </div>
+              <Button
+                className="absolute right-2 top-2"
+                variant="light"
+                leftSection={<IoCopy />}
+                color={clipboardLoc.copied ? "purple" : "red"}
+                onClick={() =>
+                  clipboardLoc.copy(clipboardRefLoc.current.innerText)
+                }
+              >
+                {clipboardLoc.copied ? "Copied" : "Copy"}
+              </Button>
+            </Alert>
+
             <Alert
               className="relative"
               variant="light"
