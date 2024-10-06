f2378eeefc9bde82434492457473c15430705437
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-10-06 22:07:49 +0100

    chore: styling unpaid report

--- frontend/src/screens/reporting/unpaid.tsx
+++ frontend/src/screens/reporting/unpaid.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import httpService from "../../common/http-service";
 import Currency from "../../components/currency";
 import { UnpaidModel } from "../../models/reports/unpaid";
+import clsx from "clsx";
 
 export default function Unpaid() {
   const {
@@ -16,13 +17,13 @@ export default function Unpaid() {
   });
 
   return (
-    <Table striped withRowBorders={false}>
+    <Table striped highlightOnHover withRowBorders={false}>
       <Table.Thead>
         <Table.Tr>
           <Table.Th>Name</Table.Th>
-          <Table.Th>Match Count</Table.Th>
-          <Table.Th>Unpaid Amount</Table.Th>
+          <Table.Th>Matches</Table.Th>
           <Table.Th>Summary</Table.Th>
+          <Table.Th>Unpaid</Table.Th>
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
@@ -33,10 +34,17 @@ export default function Unpaid() {
               <Table.Tr key={item.playerId}>
                 <Table.Td>{item.playerName}</Table.Td>
                 <Table.Td>{item.matchCount}</Table.Td>
-                <Table.Td>
+                <Table.Td>{item.registrationSummary}</Table.Td>
+                <Table.Td
+                  className={clsx(
+                    "font-bold",
+                    item.unpaidAmount > 10
+                      ? "text-rose-500"
+                      : "text-emerald-500",
+                  )}
+                >
                   <Currency value={item.unpaidAmount} />
                 </Table.Td>
-                <Table.Td>{item.registrationSummary}</Table.Td>
               </Table.Tr>
             );
           })}
